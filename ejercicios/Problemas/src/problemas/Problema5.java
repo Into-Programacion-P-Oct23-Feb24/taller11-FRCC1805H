8e82f9ab9f382415163793bb6507ae3522613e46
fabian <frcanar2@utpl.edu.ec>
2024-01-06 01:31:31 -0500

    taller11

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,24 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int sumaTotal = valorSuma(informacion);
+        System.out.printf("La suma del arreglo bidimensional es: %d\n",
+                sumaTotal);
+    }
+
+    public static int valorSuma(int[][] arreglo) {
+        int suma = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                suma = suma + arreglo[i][j];
+            }
+        }
+
+        return suma;
+    }
+
 }
--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,38 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        int[] informacion = {8, 5, 4, 6, 15, 3, 7, 33, 21};
+        double mediaAritmetica = calcularMediaritmetica(informacion);
+        System.out.println("El valor de la media aritmetica es: "
+                + mediaAritmetica);
+        double desviacion = calcularDesviacion(informacion,
+                mediaAritmetica);
+        System.out.println("El valor de la desviacion media es de: "
+                + desviacion);
+    }
+
+    public static int calcularMediaritmetica(int[] informacion) {
+        int suma = 0;
+        int media;
+        for (int i = 0; i < informacion.length; i++) {
+            suma += informacion[i];
+        }
+        media = suma / informacion.length;
+        return media;
+    }
+
+    public static double calcularDesviacion(int[] informacion,
+            double mediaArit) {
+        double sumaDiferenciasCuadrado = 0.0;
+        for (int i = 0; i < informacion.length; i++) {
+            double diferencia = informacion[i] - mediaArit;
+            sumaDiferenciasCuadrado += Math.pow(diferencia, 2);
+
+        }
+        double varianza = sumaDiferenciasCuadrado / informacion.length;
+        double desviacionEstandar = Math.sqrt(varianza);
+        return desviacionEstandar;
+    }
 }
