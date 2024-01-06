a5994baad6e129ba92c95c6970bebdb7aaaa7bd6
fabian <frcanar2@utpl.edu.ec>
2024-01-05 23:16:22 -0500

    taller

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,17 +5,65 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema2 {
 
+    static Scanner entrada = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Ingrese para Calcular"
+                + " 1 Area Cuadrado 2 Area Triangulo 3 Area Rectangulo ");
+        int opcion = entrada.nextInt();
+        if (opcion == 1) {
+            areaCuadrado();
+        } else {
+            if (opcion == 2) {
+                areaTriangulo();
+            } else {
+                if (opcion == 3) {
+                    areaRectangulo();
+                }
+            }
+
+        }
     }
-    
+
+    public static void areaCuadrado() {
+        double area;
+        System.out.println("Ingrese el valor del lado ");
+        double lado = entrada.nextDouble();
+        area = lado * lado * lado * lado;
+        System.out.printf(" Area del cuadrado es: %.2f\n", area);
+    }
+
+    public static void areaTriangulo() {
+        double area;
+        System.out.println("Ingrese el valor de la base ");
+        double base = entrada.nextDouble();
+        System.out.println("Ingrese el valor de la altura ");
+        double altura = entrada.nextDouble();
+        area = (base * altura) / 2;
+        System.out.printf(" Area del triangulo es: %.2f\n", area);
+    }
+
+    public static void areaRectangulo() {
+        double area;
+        System.out.println("Ingrese el valor de la base ");
+        double base = entrada.nextDouble();
+        System.out.println("Ingrese el valor de la altura ");
+        double altura = entrada.nextDouble();
+        area = base * altura;
+        System.out.printf(" Area del rectangulo es: %.2f\n", area);
+    }
+
 }
--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,17 +5,56 @@
  */
 package problemas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema3 {
 
+    static Scanner entrada = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        entrada.useLocale(Locale.US);
+        System.out.println("Ingrese su primera nota");
+        double n1 = entrada.nextDouble();
+        System.out.println("Ingrese su segunda nota");
+        double n2 = entrada.nextDouble();
+        System.out.println("Ingrese su tercera nota");
+        double n3 = entrada.nextDouble();
+        System.out.println("Ingrese su cuarta nota");
+        double n4 = entrada.nextDouble();
+        String cualitativo = "";
+        cualitativo = obtenerPromedio(n1, n2, n3, n4);
+        System.out.printf("El promedio de las notas: %.2f; %.2f"
+                + "; %.2f; %.2f es %s\n", n1, n2, n3, n4, cualitativo);
     }
-    
+
+    public static String obtenerPromedio(double n1, double n2, double n3, double n4) {
+        entrada.useLocale(Locale.US);
+        double promedio;
+        String cualitativo = "";
+        promedio = (n1 + n2 + n3 + n4) / 4;
+        if ((promedio >= 0) && (promedio <= 5)) {
+            cualitativo = "Regular";
+        } else {
+            if ((promedio > 5) && (promedio <= 8)) {
+                cualitativo = "Bueno ";
+            } else {
+                if ((promedio > 8) && (promedio <= 9)) {
+                    cualitativo = "Muy Bueno";
+                } else {
+                    cualitativo = "Sobresaliente";
+                }
+            }
+        }
+        return cualitativo;
+
+    }
 }
